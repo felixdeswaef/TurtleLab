f816e3938e778fbc9d76c4d9ab441407e05b5172
Ubuntu <>
2023-04-20 12:38:05 +0000

    GPIO TEST

--- ws2812b/gpio_test.py
+++ ws2812b/gpio_test.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
 
 while(1):
- GPIO.output(23, GPIO.HIGH)
+ GPIO.output(25, GPIO.HIGH)
  time.sleep(1)
- GPIO.output(23, GPIO.LOW)
+ GPIO.output(25, GPIO.LOW)
  time.sleep(1)
