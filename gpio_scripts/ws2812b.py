eef91ff5e81517bced6d6eb6cfa4a67e8556d4d0
keukenrol <tim.vancauwenbergh@hotmail.com>
2023-04-22 12:52:29 +0200

    Update ws2812b.py

--- gpio_scripts/ws2812b.py
+++ gpio_scripts/ws2812b.py
@@ -1,7 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# Simple test for NeoPixels on Raspberry Pi
 import time
 import board
 import neopixel
@@ -12,15 +8,8 @@ from adafruit_led_animation.animation.rainbow import Rainbow
 from adafruit_led_animation.sequence import AnimationSequence
 from adafruit_led_animation.color import PURPLE, AMBER, JADE, WHITE
 
-# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
-# NeoPixels must be connected to D10, D12, D18 or D21 to work.
 pixel_pin = board.D18
-
-# The number of NeoPixels
 num_pixels = 34
-
-# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
-# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
@@ -33,37 +22,6 @@ chase = Chase(pixels, speed=0.1, size=4, spacing=6, color=AMBER)
 rainbow = Rainbow(pixels, speed=0.1, period=2)
 
 animations = AnimationSequence(blink, comet, chase, rainbow, advance_interval=5, auto_clear=True)
-
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
-
-
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
-        pixels.show()
-        time.sleep(wait)
-
 
 while True:
     animations.animate()
