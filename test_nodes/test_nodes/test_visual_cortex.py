da7305856004a2c2252113c0c059111122eebe61
Olivier <ovanlier@outlook.com>
2023-04-22 13:42:25 +0200

    test publisher for camera_info topic

---
+++ test_nodes/test_nodes/test_visual_cortex.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class test_visual_cortex(Node):
+    
+    #value's for anlge, distance and detected
+    angle = 3.7
+    distance = 1.1
+    detected = 0
+    
+    def __init__(self):
+        super().__init__(type(self).__name__) #give this node the name of the class
+        
+        #create test publisher
+        self.test_publisher = self.create_publisher(
+            String,      #msg type
+            '/camera_info', #topic name 
+             10         #qos
+        )
+        #send command every timer_period seconds
+        timer_period = 0.1
+        self.timer = self.create_timer(timer_period, self.publish_camera_info)
+        
+
+    def publish_camera_info(self):
+        #create msg
+        msg = String()
+        msg += String(self.angle)
+        msg += ";" 
+        msg += String(self.distance)
+        msg += ";"
+        msg += String(self.detected)
+        #publish msg
+        self.test_publisher.publish(msg)
+        self.get_logger().info(f"Publishing a msg: {msg}")
+
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    #make new publisher object
+    test_publisher = test_visual_cortex()
+    #keep node alive
+    rclpy.spin(test_publisher)
+
+    test_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
