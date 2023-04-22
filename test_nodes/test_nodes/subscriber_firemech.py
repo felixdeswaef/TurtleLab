73e7b50e9f27c782e908e6ee8693aa90379084ca
YeniFish <65500803+YeniFish@users.noreply.github.com>
2023-04-22 13:16:26 +0200

    Update subscriber_firemech.py

--- test_nodes/test_nodes/subscriber_firemech.py
+++ test_nodes/test_nodes/subscriber_firemech.py
@@ -12,7 +12,7 @@ Lader = 12
 GPIO.setup(Motoren, GPIO.OUT)
 GPIO.setup(Lader, GPIO.OUT)
 servoLader_pwm = GPIO.PWM(Lader,50)
-servoLader_pwm.start(2.5)
+servoLader_pwm.start(6)
 
 class MinimalSubscriber(Node):
 
@@ -26,9 +26,9 @@ class MinimalSubscriber(Node):
         if msg.data == 'activeren':
             #GPIO.output(Motoren, GPIO.HIGH)
             time.sleep(2)
-            servoLader_pwm.ChangeDutyCycle(5)
-            time.sleep(0.5)
-            servoLader_pwm.ChangeDutyCycle(2.5)
+            #servoLader_pwm.ChangeDutyCycle(5)
+            #time.sleep(0.5)
+            #servoLader_pwm.ChangeDutyCycle(2.5)
         
         elif msg.data == 'deactiveren':
             GPIO.output(Motoren, GPIO.LOW)
