474a33b810ceea0494265bfa5dcab769472d0796
DrKush420 <kobevandooren@gmail.com>
2023-04-26 13:44:51 +0200

    visual changes

--- test_nodes/test_nodes/visual_cortex.py
+++ test_nodes/test_nodes/visual_cortex.py
@@ -3,6 +3,7 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 from math import sqrt
+from math import tan
 from std_msgs.msg import String
 
 class Visual_Cortex(Node):
@@ -14,9 +15,9 @@ class Visual_Cortex(Node):
         self.cm = np.array([[823.93985557  , 0.      ,   322.76228491],[  0.    ,     825.11141958 ,279.6240493 ],[  0.    ,       0.      ,     1.        ]])
         self.parameters = cv2.aruco.DetectorParameters()
         self.ms=0.05
+        self.enemy=[0,1,2,3]
         self.detector = cv2.aruco.ArucoDetector(self.dictionary, self.parameters)
         self.dm = np.array([[ 6.29137073e-02 ,-7.33484417e-01  ,6.53444356e-03 , 3.83894903e-03, 1.16325776e+01]])
-        self.hoek=45            #nog te testen 
         self.timer = self.create_timer(0.3, self.timer_callback)  # process the vid every 1 second
     def my_estimatePoseSingleMarkers(self,corners):
         marker_size=self.ms
@@ -39,25 +40,27 @@ class Visual_Cortex(Node):
     def pose_estimation(self,frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         corners, ids, rejected_img_points = self.detector.detectMarkers(gray)
-        param=2.4
         enemy=False
+        enemyid=[0,1,2,3]
         if len(corners) > 0:    
-            for i in range(0, len(ids)):
+            for i, id in enumerate(ids):
+                if id not in enemyid:
+                    continue
                 rvec, tvec, rejimp= self.my_estimatePoseSingleMarkers(corners[i])
                 if len(tvec)!=0:
-                    afstand="{:.3f}".format(param*sqrt(tvec[0][0]**2+tvec[0][2]**2))             
-                    h=corners[i][0][1]-corners[i][0][0]*self.hoek
-                    return afstand,h
-        return 0,0
+                    afstand=sqrt(tvec[0][0]**2+tvec[0][2]**2) 
+                    h=tan(tvec[0][0]/tvec[0][2])       
+                    return afstand,h,1
+        return 0,0,0
 
     def timer_callback(self):
         ret, frame = self.camera.read()  # read a frame from the camera
         if not ret:
             self.get_logger().warning('Failed to read frame from camera')
             return
-        var3=self.pose_estimation(frame)
+        var1,var2,var3=self.pose_estimation(frame)
         msg=String()
-        msg.data = str(var3)
+        msg.data = str(var1)+";"+str(var2)
         self.publisher_.publish(msg)
         
 
