a2bc5372943d73584235191d3e98c3e6a4cde56e
keukenrol <tim.vancauwenbergh@hotmail.com>
2023-05-02 12:41:59 +0200

    Update ledsub.py

--- test_nodes/test_nodes/ledsub.py
+++ test_nodes/test_nodes/ledsub.py
@@ -19,18 +19,18 @@ GPIO.setup(25, GPIO.OUT)
 bluestate = 0
 
 # animation and color library
-# from adafruit_led_animation.color import *
-# from adafruit_led_animation.animation.rainbow import Rainbow
-# from adafruit_led_animation.animation.chase import Chase
-# from adafruit_led_animation.animation.comet import Comet
-# from adafruit_led_animation.animation.solid import Solid
-# from adafruit_led_animation.color import *
+from adafruit_led_animation.color import *
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.animation.chase import Chase
+from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.solid import Solid
+from adafruit_led_animation.color import *
 
 # define animations
-# rainbow = Rainbow(pixels, speed=0.1, period=3, step=5)
-# comet = Comet(pixels, speed=0.1, color=PURPLE, tail_length=17, bounce=True)
-# chase = Chase(pixels, speed=0.1, size=4, spacing=6, color=AMBER)
-# solid = Solid(pixels, color=RED)
+rainbow = Rainbow(pixels, speed=0.1, period=3, step=5)
+comet = Comet(pixels, speed=0.1, color=PURPLE, tail_length=17, bounce=True)
+chase = Chase(pixels, speed=0.1, size=4, spacing=6, color=AMBER)
+solid = Solid(pixels, color=RED)
 
 class pixelNode(Node):
 
@@ -59,29 +59,29 @@ class pixelNode(Node):
         
         if msg.data == "driving":
             bluestate = 0
-            self.pixels.fill((0, 255, 0))
+            #self.pixels.fill((0, 255, 0))
             #self.get_logger().info('LEDS: DRIVING')
             
         elif msg.data == "detected":
             bluestate = 0
-            self.pixels.fill((255, 127, 0))
+            #self.pixels.fill((255, 127, 0))
             #self.get_logger().info('LEDS: DETECTED')
             
         elif msg.data == "shoot":
             bluestate = 1
-            self.pixels.fill((255, 0, 255))
+            #self.pixels.fill((255, 0, 255))
             #self.get_logger().info('LEDS: SHOOT')
 
         else:
             bluestate = 0
-            self.pixels.fill((255, 0, 0))
+            #self.pixels.fill((255, 0, 0))
             #self.get_logger().info('LEDS: UNKNOWN')
         
         self.pixels.show()
         
     def timer_callback(self):
         global bluestate
-        
+        rainbow.animate()
         if bluestate == 0:
             GPIO.output(25, GPIO.HIGH)
         else:
