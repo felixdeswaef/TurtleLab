1757ec385faa12dd322cac3bd2af3cbd3f35b538
keukenrol <tim.vancauwenbergh@hotmail.com>
2023-05-02 10:48:23 +0200

    Update ws2812b.py

--- gpio_scripts/ws2812b.py
+++ gpio_scripts/ws2812b.py
@@ -6,7 +6,6 @@ import time
 import board
 import neopixel
 
-
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
 # NeoPixels must be connected to D10, D12, D18 or D21 to work.
 pixel_pin = board.D10
@@ -19,7 +18,7 @@ num_pixels = 34
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+    pixel_pin, num_pixels, brightness=0.5, auto_write=False, pixel_order=ORDER
 )
 
 
