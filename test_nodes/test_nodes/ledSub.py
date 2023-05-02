7552c20863514d8696a0e98924b4efce28922c2f
keukenrol <tim.vancauwenbergh@hotmail.com>
2023-05-02 10:33:14 +0200

    Update ledSub.py

--- test_nodes/test_nodes/ledSub.py
+++ test_nodes/test_nodes/ledSub.py
@@ -5,6 +5,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+
 import RPi.GPIO as GPIO
 import time
 
@@ -13,9 +14,13 @@ import time
 import board
 import neopixel
 
+# toggle blue LED every 500ms
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)
+# state of LED
+bluestate = 0
 
+# animation and color library
 from adafruit_led_animation.color import *
 from adafruit_led_animation.animation.rainbow import Rainbow
 from adafruit_led_animation.animation.chase import Chase
@@ -23,7 +28,7 @@ from adafruit_led_animation.animation.comet import Comet
 from adafruit_led_animation.animation.solid import Solid
 from adafruit_led_animation.color import *
 
-bluestate = 0
+# define animations
 rainbow = Rainbow(pixels, speed=0.1, period=3, step=5)
 comet = Comet(pixels, speed=0.1, color=PURPLE, tail_length=17, bounce=True)
 chase = Chase(pixels, speed=0.1, size=4, spacing=6, color=AMBER)
@@ -45,29 +50,30 @@ class pixelNode(Node):
         self.pixels.show()
         
         # blue led blinker
-        timer_period = 0.5  # 2Hz
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        blue_tim_per = 0.5  # 2Hz
+        self.blue_tim = self.create_timer(timer_period, self.timer_callback)
         
         self.pixels.fill((255, 255, 255))
         self.get_logger().info('LEDS: STARTUP')
         self.pixels.show()
-
+    
+    # Use static colors initially
     def listener_callback(self, msg):        
         if msg.data == 'driving':
             self.pixels.fill((0, 255, 0))
-            self.get_logger().info('LEDS: DRIVING')
+            #self.get_logger().info('LEDS: DRIVING')
             
         elif msg.data == 'detected':
             self.pixels.fill((255, 127, 0))
-            self.get_logger().info('LEDS: DETECTED')
+            #self.get_logger().info('LEDS: DETECTED')
             
         elif msg.data == 'shoot':
             self.pixels.fill((255, 0, 255))
-            self.get_logger().info('LEDS: SHOOT')
+            #self.get_logger().info('LEDS: SHOOT')
 
         else:
             self.pixels.fill((255, 0, 0))
-            self.get_logger().info('LEDS: UNKNOWN')
+            #self.get_logger().info('LEDS: UNKNOWN')
         
         self.pixels.show()
         
