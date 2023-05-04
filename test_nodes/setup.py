d061d4a268f1ab8f5205eb07a8c6102dcaa2dbf7
Olivier <ovanlier@outlook.com>
2023-05-04 17:17:30 +0200

    pkg problem fix

--- test_nodes/setup.py
+++ test_nodes/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['launch/test_launch.py'] ),
+        ('share/' + package_name, ['launch/test_launch.py', 'launch/test_launch_firemech2.py', 'launch/test_launch_spideysense.py',] )
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,15 +21,12 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            #main nodes
             'visual_cortex = test_nodes.visual_cortex:main',   
             'movement_controller = test_nodes.movement_controller:main',
             'ledSub = test_nodes.ledSub:main',   
             'firemech = test_nodes.firemech:main',   
-            #variations of firemech
             'firemech_2 = test_nodes.firemech_2:main', 
-            'spideysense = test_nodes.spideysense:main',  
-            #testing nodes   
+            'spideysense = test_nodes.spideysense:main',   
         	'talker = test_nodes.publisher_firemech_test:main',
             'vel_publisher = test_nodes.vel_publisher:main',
             'keyboard_reader = test_nodes.keyboard_reader:main',
--- turtlebot3_nodes/setup.py
+++ turtlebot3_nodes/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['launch/launch.py'] ),
+        ('share/' + package_name, ['launch/launch.py', 'launch/launch_firemech2.py', 'launch/launch_spideysense.py','launch/launch_debug.py', 'launch/launch_debug2.py'] )
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,12 +21,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            #main nodes
             'visual_cortex = turtlebot3_nodes.visual_cortex:main',   
             'movement_controller = turtlebot3_nodes.movement_controller:main',
             'ledSub = turtlebot3_nodes.ledSub:main',   
             'firemech = turtlebot3_nodes.firemech:main',   
-            #variations of firemech
             'firemech_2 = turtlebot3_nodes.firemech_2:main', 
             'spideysense = turtlebot3_nodes.spideysense:main'
         ],
